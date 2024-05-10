827a2af92ca277dd2d5f108f3d310e8495f4f341
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2024-05-10 18:38:36 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,30 @@
-// JavaScript source code
+//configurações tela do jogo
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 300,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+// Criação do jogo
+var game = new Phaser.Game(config);
+
+// Captura as teclas pressionadas
+var cursors;
+// Função de pré-carregamento de imagens sons e etc
+function preload() {
+    
+}
+//carregamento de variáveis de predefinições logo antes de iniciar o jogo
+function create() {
+    cursors = this.input.keyboard.createCursorKeys();
+
+}
+// Função de atualização , execução quadro a quadro
+function update() {
+
+}

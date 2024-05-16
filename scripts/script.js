3f30e1af91c87a116c6e285fa1da7f9d073666c3
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2024-05-16 20:19:02 -0300

    Update script.js
    exemplo de botão html na tela do phaser

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,22 @@ class MainMenu extends Phaser.Scene {
     }
     //carregar informações iniciais da cena
     create() {
-        
+        //instancia botão na tela, na div criada no index game-conteiner
+        var button = document.createElement('button');
+        button.innerHTML = 'Play';
+        button.id = 'my-button';
+        button.style.position = 'absolute';
+        button.style.transform = 'translateX(350%)';
+        button.style.padding = '10px 20px';
+        button.style.fontSize = '16px';
+        //adicionando na div
+        document.getElementById('game-container').appendChild(button);
+        //referencia do ambiente
+        var ref = this;
+        //ação do botão
+        button.addEventListener('click', function () {
+            text = ref.add.text(100, 100, 'test');
+        });
     }
     //informações que serão carregadas a cada frame
     update() {

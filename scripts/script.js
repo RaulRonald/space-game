09815fcb3a8d818bfc684b0908e61c6adb4a3a96
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2024-05-16 19:48:39 -0300

    Update script.js
    coloquei de cena main menu e comentei as parada

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,24 @@
+//cena do menu principal
+class MainMenu extends Phaser.Scene {
+    //carregar imagens sons gifs videos que serao usados
+    preload() {
+
+    }
+    //carregar informações iniciais da cena
+    create() {
+        
+    }
+    //informações que serão carregadas a cada frame
+    update() {
+
+    }
+}
 //configurações tela do jogo
 var config = {
     type: Phaser.AUTO,
-    width: 600,
-    height: 300,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+    width: 1280,
+    height: 720,
+    scene: [MainMenu]
 };
+const game = new Phaser.Game(config);
 
-// Criação do jogo
-var game = new Phaser.Game(config);
-
-// Captura as teclas pressionadas
-var cursors;
-// Função de pré-carregamento de imagens sons e etc
-function preload() {
-    
-}
-//carregamento de variáveis de predefinições logo antes de iniciar o jogo
-function create() {
-    cursors = this.input.keyboard.createCursorKeys();
-
-}
-// Função de atualização , execução quadro a quadro
-function update() {
-
-}

2e39f49f136c36f94970dd4bafafa8048aff385d
Raul Ronald <108026640+RaulRonald@users.noreply.github.com>
2024-05-23 22:32:26 -0300

    ativando ataque do jogador

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -1,30 +1,69 @@
 
 export class PlayerSpace extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y, texture, frame) {
+    
+    constructor(sabertexture,slashtexture,scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         //colocando a origem do jogador
         this.setOrigin(0, 0);
         //setando a sua velocidade
+        let ref = this;
         this.playervelocity = 3;
+        this.saber_dir = 1;
+        //carregando objetos que serão usados no ataque básico
+        this.saber = new Phaser.Physics.Arcade.Sprite(scene, x, y, sabertexture);
+        scene.add.existing(this.saber);
+        this.slash = new Phaser.Physics.Arcade.Sprite(scene,x, y, slashtexture);
+        this.slash.visible = false;
+        scene.add.existing(this.slash);
+        //ataque ao clicar
+        scene.input.on('pointerdown', function () {
+            //ataque em sí
+            if (ref.saber.visible == true) {
+                ref.saber.visible = false;
+                ref.slash.visible = true;
+            }
+            //cooldawn do ataque
+            ref.scene.time.delayedCall(500, function () {
+                ref.saber.visible = true;
+                ref.slash.visible = false;
+            }, [], ref.scene);
+            });
     }
     update() {
         //função de movimento do jogador
         this.move();
+        this.atack();
+    }
+    atack() {
+        this.saber.x = this.x + (this.saber_dir * 35);
+        this.saber.y = this.y + 15;
+        this.slash.x = this.x + (this.saber_dir * 70);
+        this.slash.y = this.y + 5;
     }
     move() {
         const scene = this.scene;
         //movimento com base no cursor
         if (scene.cursors.down.isDown) {
             this.y += this.playervelocity;
+            
         }
         if (scene.cursors.up.isDown) {
             this.y -= this.playervelocity;
         }
         if (scene.cursors.right.isDown) {
             this.x += this.playervelocity;
+            this.saber_dir = 1;
+            this.slash.flipX = false;
+            this.saber.flipX = false;
+            this.flipX = false;
         }
         if (scene.cursors.left.isDown) {
             this.x -= this.playervelocity;
+            this.saber_dir = -0.25;
+            this.slash.flipX = true;
+            this.saber.flipX = true;
+            this.flipX = true;
+
         }
         //colisão com as bordas do jogo, vemos isso na config do jogo
         if (this.y <= 0) this.y += this.playervelocity;
--- public/scripts/spacegame.js
+++ public/scripts/spacegame.js
@@ -12,6 +12,8 @@ export class Spacegame extends Phaser.Scene {
     preload() {
         //carregando os sprites
         this.load.image('player', 'data/assets/sprites/player-placeholder.png');
+        this.load.image('saber', 'data/assets/sprites/saber-placeholder.png');
+        this.load.image('slash', 'data/assets/sprites/slash-placeholder.png');
         this.load.image('map', 'data/assets/images/grass_battle_area.png');
     }
     create() {
@@ -20,14 +22,14 @@ export class Spacegame extends Phaser.Scene {
         //aderindo a variável o controle do teclado
         this.cursors = this.input.keyboard.createCursorKeys();
         //aderindo a variavel as informações da classe do jogador
-        this.playerspace = new PlayerSpace(this, 0, 0,'player', 0);
+        this.playerspace = new PlayerSpace('saber','slash',this, 0, 0,'player', 0);
         this.add.existing(this.playerspace);
         //configurações da camera principal
         this.cameras.main.setBounds(0, 0, 720, 360);
         //camera seguir o jogador
         this.cameras.main.startFollow(this.playerspace, true);
         //zoom da camera
-        this.cameras.main.setZoom(1.5);
+        this.cameras.main.setZoom(1.25);
     }
     update() {
         //update do jogador
